ed459bfb945c435d8a204cf7076b780e31a488a8
jmando01 <jmando01@hotmail.com>
2015-04-30 00:12:07 -0400

    Initial commit

---
+++ app/src/main/java/com/example/vending/vending/LoginActivity.java
@@ -0,0 +1,112 @@
+package com.example.vending.vending;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class LoginActivity extends ActionBarActivity {
+
+    private EditText usernameEdit;
+    private EditText passwordEdit;
+    private String username;
+    private String password;
+    private String DBpassword;
+    private ProgressDialog Asycdialog;
+    private Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        context = this;
+    }
+
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo ni = cm.getActiveNetworkInfo();
+        if (ni == null) {
+            // There are no active networks.
+            return false;
+        } else
+            return true;
+    }
+
+    public void loginbtn(View view){
+
+        if(!(isNetworkConnected())){
+            Asycdialog.dismiss();
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+            alertDialogBuilder.setTitle("No Network Avaliable!");
+            alertDialogBuilder
+                    .setMessage("Your device is not connected to the Internet. Please check your connection and try again.")
+                    .setCancelable(false)
+                    .setNeutralButton("Got it!",new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog,int id) {
+                            dialog.cancel();
+                        }
+                    });
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+
+        }else {
+
+            Asycdialog = new ProgressDialog(LoginActivity.this);
+            Asycdialog.setMessage("Logging In...");
+            Asycdialog.setCanceledOnTouchOutside(false);
+            Asycdialog.show();
+
+            usernameEdit = (EditText) findViewById(R.id.username);
+            passwordEdit = (EditText) findViewById(R.id.password);
+            username = usernameEdit.getText().toString();
+            password = passwordEdit.getText().toString();
+
+            if(DBpassword.equals(GetPasswordWithUsername(username))){
+                Asycdialog.dismiss();
+                //Next Activity
+            }else{
+                Asycdialog.dismiss();
+                //Wrong Credentials
+            }
+        }
+
+    }
+
+    public String GetPasswordWithUsername(String username){
+        DBpassword = " ";
+
+        return DBpassword;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
